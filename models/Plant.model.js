d228eb63983e5158caeb4dab982112e148d6a246
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-03-07 22:42:20 +0100

    added new property and placeholder picture

--- models/Plant.model.js
+++ models/Plant.model.js
@@ -3,13 +3,11 @@ const mongoose = require("mongoose");
 
 const plantSchema = new Schema({
   name: { type: String, required: true },
-  image: {
-    type: String,
-    default: "https://avalos.sv/wp-content/uploads/default-featured-image.png",
-  },
+  image: {type: String, default: "https://www.nycstreetdesign.info/sites/default/files/2018-12/Leaf%20icon_14.jpg" },
   location: { type: String, enum: ["interior", "exterior"] },
   directSunlight: { type: Boolean, default: false },
   toxicity: { type: Boolean, default: false },
+  difficulty: {type: String, enum: ["Easy Care", "High Maintenance"]},
   carePlan: { type: mongoose.Schema.Types.ObjectId, ref: "Careplan" },
 });
 
